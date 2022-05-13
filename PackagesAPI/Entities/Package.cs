06442bcd7d97b0c8336d66338f3222c82fef752b
Paulo Monezi <paulo.monezi@icloud.com>
2022-05-13 10:34:36 -0400

    Project created

---
+++ PackagesAPI/Entities/Package.cs
@@ -0,0 +1,12 @@
+﻿namespace PackagesAPI.Entities
+{
+    public class Package
+    {
+        public int Id { get; private set; }
+        public string Code { get; private set; }
+        public decimal Weight { get; private set; }
+        public bool Delivered { get; private set; }
+        public DateTime PostedAt { get; private set; }        
+    }
+
+}
